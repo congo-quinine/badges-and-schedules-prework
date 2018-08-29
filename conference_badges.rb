ec1e88df74c75a819deca914b11ca2ad92e3f14a
Rob Parker <into.the.parker@gmail.com>
2018-08-29 10:38:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+# Write your code here.\
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array_badges = []
+    attendees.each do |i|
+      array_badges.push("Hello, my name is #{i}.")
+  end
+  return printer(array_badges)
+end
+
+
+def assign_rooms(attendees)
+  roomListArray = []
+    roomNumber = 1
+      attendees.each do |i|
+        roomListArray.push("Hello, #{i}! You'll be assigned to room #{roomNumber}!")
+          roomNumber += 1
+      end
+    return printer(roomListArray)
+end
+
+def printer(attendees)
+    attendees.each do |i|
+      puts i
+    end
+end
